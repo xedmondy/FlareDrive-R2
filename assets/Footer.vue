a60ee66d821f8a1591e1efd48cee4e03bf773a45
tadashishoji <tadashishoji@tadashishojideiMac.local>
2026-05-18 13:03:21 +0800

    chore: remove project links from footer

--- assets/Footer.vue
+++ assets/Footer.vue
@@ -1,15 +1,6 @@
 <template>
   <footer class="footer">
 
-    <div class="project-links">
-      <strong>Project:</strong><a href="https://github.com/willow-god/FlareDrive-R2" target="_blank"
-        rel="noopener noreferrer">FlareDrive-R2</a>
-      <strong>Author:</strong><a href="https://github.com/willow-god" target="_blank"
-        rel="noopener noreferrer">LiuShen</a>
-      <strong>Forked from:</strong><a href="https://github.com/ljxi/Cloudflare-R2-oss" target="_blank"
-        rel="noopener noreferrer">Cloudflare-R2-OSS</a>
-    </div>
-
     <div class="powered">Powered by <a href="https://workers.cloudflare.com/" target="_blank"
         rel="noopener noreferrer">Cloudflare Workers</a></div>
 
@@ -33,25 +24,6 @@ export default {
   font-size: 16px;
   border-radius: 50% 50% 0 0;
 
-}
-
-.project-links {
-  font-size: 14px;
-  color: #444;
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-  gap: 15px;
-  text-align: center;
-}
-
-.project-links a {
-  color: #444;
-  text-decoration: none;
-}
-
-.project-links a:hover {
-  color: #222;
 }
 
 .powered {
